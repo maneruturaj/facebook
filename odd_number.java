64065a765f0798e8c832b2c569697e10ec155aa4
maneruturaj <124126515+maneruturaj@users.noreply.github.com>
2023-03-29 14:19:01 +0530

    Create odd_number.java

---
+++ odd_number.java
@@ -0,0 +1,18 @@
+package testpackage1;
+
+public class Odd_number {
+
+	public static void main(String[] args) {
+		int k=20;
+		for(int o=1;o<=k;o++)
+		{
+			if(o%2==0)
+			{
+				System.out.println("Odd num is "+ o);
+			}
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
