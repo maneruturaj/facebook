722e4dde8cff9fea7b6814c11f9c89982b55b29e
Ruturaj Mane <mane.ruturaj4@gmail.com>
2023-04-05 13:51:19 +0530

    heloo hashset1

---
+++ HashSet1.java
@@ -0,0 +1,40 @@
+package tests;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+
+public class HashSet1 
+{
+
+	public static void main(String[] args) 
+	{
+		 HashSet<Integer> s = new HashSet<Integer>();
+		
+		s.add(56);
+		s.add(89);
+		s.add(76);
+		s.add(87);
+		s.add(65);
+		
+		for(int x : s)
+		{
+			System.out.println(x);
+		}
+		
+		Iterator<Integer>  i = s.iterator();
+			
+		i.next();
+		i.hasNext();
+		for(int x : s) //by using while loop data retreval
+		{
+			System.out.println(i.next());
+		}
+		
+		while(i.hasNext()) //by using while loop data retreval 
+		{
+			System.out.println(i.next());
+		}
+	}
+
+}
